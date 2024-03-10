fa89565c96481e1798175a4e6fc3faec142ced8a
Fernandagsilva <fgsilva@ice.ufjf.br>
2024-03-10 15:34:52 -0300

    start setuo clj-kondo

--- test/cine_flix/core_test.clj
+++ test/cine_flix/core_test.clj
@@ -1,10 +1,10 @@
 (ns cine-flix.core-test
-  (:require [clojure.test :refer :all]
-            [cine-flix.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [cine-flix.core :as cine-flix]))
 
 (deftest add-two-numbers-test
   (testing "should add two numbers"
-    (is (= 5 (cine-flix.core/add-two-numbers 2 3)))
-    (is (= 5 (cine-flix.core/add-two-numbers 3 2))))
+    (is (= 5 (cine-flix/add-two-numbers 2 3)))
+    (is (= 5 (cine-flix/add-two-numbers 3 2))))
   (testing "shouldn't add two number"
-    (is (thrown? Exception (cine-flix.core/add-two-numbers 3 nil)))))
+    (is (thrown? Exception (cine-flix/add-two-numbers 3 nil)))))
